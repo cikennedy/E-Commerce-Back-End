c491b30b13aae26a044cdc741820efd28e571a51
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-05-03 16:40:57 -0500

    added code to delete product by id

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -67,6 +67,19 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const catData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
 // update product
 router.put('/:id', (req, res) => {
   // update product data
@@ -109,8 +122,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id.' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
