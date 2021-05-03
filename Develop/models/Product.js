7991f8e4db48fc71a35ec257e57725c5282ab67f
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-05-03 15:57:22 -0500

    updated product model based on the product-seeds

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,25 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
